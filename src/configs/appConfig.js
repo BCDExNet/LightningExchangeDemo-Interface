2413b451f32ba708bbb3999f23f984ea80f064c5
Yushin Si <siyushin@hotmail.com>
2023-04-09 16:21:50 +0800

    support KCC, REI and Iotex...

--- src/configs/appConfig.js
+++ src/configs/appConfig.js
@@ -3,59 +3,86 @@ export const appConfig = {
 	fee: 100,
 	btcLimit: 50000,
 	updateDurationMS: 600000,
-	priceApi: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&sparkline=false&page=1&ids=bitcoin,usd-coin,binance-usd,binance-bitcoin,tether,binancecoin,wrapped-bitcoin,elastos,ethereum",
+	priceApi: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&sparkline=false&page=1&ids=bitcoin,usd-coin,binance-usd,binance-bitcoin,tether,binancecoin,wrapped-bitcoin,elastos,ethereum,kingcorgi-chain,rei-network,iotex",
 	defaultNetwork: "bsc",
 	networks: {
 		bsc: {
 			chainName: "Binance",
 			chain: "BSC",
 			chainId: 56,
-			rpcUrls: [
-				"https://bsc-dataseed1.defibit.io/"
-			],
+			rpcUrls: ["https://bsc-dataseed1.defibit.io/"],
 			nativeCurrency: {
 				name: "BINANCE",
 				symbol: "BSC",
 				decimals: 18
 			},
-			blockExplorerUrls: [
-				"https://bscscan.com/"
-			],
+			blockExplorerUrls: ["https://bscscan.com/"],
 			iconUrls: ["/images/bsc.svg"]
 		},
 		elastos: {
 			chainName: "Elastos",
 			chain: "ESC",
 			chainId: 20,
-			rpcUrls: [
-				"https://api.elastos.io/esc"
-			],
+			rpcUrls: ["https://api.elastos.io/esc"],
 			nativeCurrency: {
 				name: "ELA",
 				symbol: "ELA",
 				decimals: 18
 			},
-			blockExplorerUrls: [
-				"https://eth.elastos.io/"
-			],
+			blockExplorerUrls: ["https://eth.elastos.io/"],
 			iconUrls: ["/images/esc.svg"]
 		},
 		arbitrum: {
 			chainName: "Arbitrum",
 			chain: "Arbitrum",
 			chainId: 42161,
-			rpcUrls: [
-				"https://arb1.arbitrum.io/rpc"
-			],
+			rpcUrls: ["https://arb1.arbitrum.io/rpc"],
 			nativeCurrency: {
 				name: "ETH",
 				symbol: "ETH",
 				decimals: 18
 			},
-			blockExplorerUrls: [
-				"https://arbiscan.io/"
-			],
+			blockExplorerUrls: ["https://arbiscan.io/"],
 			iconUrls: ["/images/arbitrum.svg"]
+		},
+		kcc: {
+			chainName: "KCC",
+			chain: "KCC",
+			chainId: 321,
+			rpcUrls: ["https://rpc-mainnet.kcc.network"],
+			nativeCurrency: {
+				name: "KCS",
+				symbol: "KCS",
+				decimals: 18
+			},
+			blockExplorerUrls: ["https://explorer.kcc.io"],
+			iconUrls: ["/images/header-network-kcc.svg"]
+		},
+		rei: {
+			chainName: "REI",
+			chain: "REI",
+			chainId: 47805,
+			rpcUrls: ["https://rpc.rei.network"],
+			nativeCurrency: {
+				name: "REI",
+				symbol: "REI",
+				decimals: 18
+			},
+			blockExplorerUrls: ["https://scan.rei.network"],
+			iconUrls: ["/images/rei.svg"]
+		},
+		iotex: {
+			chainName: "IoTeX",
+			chain: "IoTeX",
+			chainId: 4689,
+			rpcUrls: ["https://babel-api.mainnet.iotex.io"],
+			nativeCurrency: {
+				name: "IOTX",
+				symbol: "IOTX",
+				decimals: 18
+			},
+			blockExplorerUrls: ["https://iotexscan.io"],
+			iconUrls: ["/images/header-network-iotex.svg"]
 		}
 	},
 	exchanges: {
@@ -163,6 +190,63 @@ export const appConfig = {
 			},
 			safeBoxNative: {
 				address: "0xeDF9AE3Dfa601ec70085ed7c898D0553b6450F08",
+				abi: "/abis/LightningSwapNative.json"
+			}
+		},
+		"321": {
+			b2cTaker: "0xf495e080adcc153579423a3860801a4e282b26f2",
+			tokens: {
+				kcc: {
+					isNative: true,
+					name: "KCC",
+					decimals: 18,
+					logo: "/images/kcs.png"
+				}
+			},
+			safeBox: {
+				address: "0x3fa4Fbb9e59A0fe2F633ce00660b7Fa5Eb548c64",
+				abi: "/abis/safe_box.json"
+			},
+			safeBoxNative: {
+				address: "0xCe5fbC259b9909A776ae60bCC2a92D6c7F70b2C3",
+				abi: "/abis/LightningSwapNative.json"
+			}
+		},
+		"47805": {
+			b2cTaker: "0xf495e080adcc153579423a3860801a4e282b26f2",
+			tokens: {
+				rei: {
+					isNative: true,
+					name: "REI",
+					decimals: 18,
+					logo: "/images/rei.svg"
+				}
+			},
+			safeBox: {
+				address: "0x9AB67c40c5A6E45b363e6bC33fF2645a12A585e1",
+				abi: "/abis/safe_box.json"
+			},
+			safeBoxNative: {
+				address: "0xD1dd77Ec76Bc2Ba9Ab987B8719d0B2527d741ecC",
+				abi: "/abis/LightningSwapNative.json"
+			}
+		},
+		"4689": {
+			b2cTaker: "0xf495e080adcc153579423a3860801a4e282b26f2",
+			tokens: {
+				iotx: {
+					isNative: true,
+					name: "IoTeX",
+					decimals: 18,
+					logo: "/images/iotx.svg"
+				}
+			},
+			safeBox: {
+				address: "0x10d0B5104EAd7D02edfae38dea77474627e0Ea57",
+				abi: "/abis/safe_box.json"
+			},
+			safeBoxNative: {
+				address: "0xeA24c4b9FF56d0867530F3401262CE84aBc11F7d",
 				abi: "/abis/LightningSwapNative.json"
 			}
 		}
